8f7c3940650d124d37c43a1e3fdf6ec35181f83f
Panu Kalliokoski <panu.kalliokoski@server.fake>
2013-09-10 13:47:48 +0300

    default namespaces, namespaced attributes

--- ckanext/oaipmh/importcore.py
+++ ckanext/oaipmh/importcore.py
@@ -6,10 +6,28 @@ from oaipmh.metadata import MetadataRegistry
 from oaipmh.common import Metadata
 from lxml import etree
 
-def namespaced_name(element):
-	name = element.tag
-	default_namespaces = []
-	for prefix, nsurl in element.nsmap.items() + default_namespaces:
+default_namespaces = [
+	('dc', 'http://purl.org/dc/elements/1.1/'),
+	('dct', 'http://purl.org/dc/terms/'),
+	('xsd', 'http://www.w3.org/2001/XMLSchema#'),
+	('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+	('rdfs', 'http://www.w3.org/2000/01/rdf-schema#'),
+	('skos', 'http://www.w3.org/2004/02/skos/core#'),
+	('owl', 'http://www.w3.org/2002/07/owl#'),
+	('nrd', 'http://purl.org/net/nrd#'),
+	('void', 'http://rdfs.org/ns/void#'),
+	('foaf', 'http://xmlns.com/foaf/0.1/'),
+	('dcat', 'http://www.w3.org/ns/dcat#'),
+	('fp', 'http://downlode.org/Code/RDF/File_Properties/schema#'),
+	('arpfo', 'http://vocab.ox.ac.uk/projectfunding#'),
+	('org', 'http://www.w3.org/ns/org#'),
+	('lvont', 'http://lexvo.org/ontology#'),
+	('qb', 'http://purl.org/linked-data/cube#'),
+	('prov', 'http://www.w3.org/ns/prov#'),
+]
+
+def namespaced_name(name, namespaces):
+	for prefix, nsurl in namespaces.items() + default_namespaces:
 		if prefix is None: prefix = ''
 		else: prefix += ':'
 		oldprefix = '{%s}' % nsurl
@@ -21,16 +39,18 @@ def generic_xml_metadata_reader(xml_element):
 	def flatten_with(prefix, element, result):
 		if element.text: result[prefix] = element.text
 		for attr in element.attrib:
-			result["%s.@%s" % (prefix, attr)] = element.attrib[attr]
+			name = namespaced_name(attr, element.nsmap)
+			result["%s.@%s" % (prefix, name)] = element.attrib[attr]
 		indices = {}
 		for child in element:
-			name = namespaced_name(child)
+			name = namespaced_name(child.tag, child.nsmap)
 			index = indices.get(name, 0)
 			indices[name] = index + 1
 			child_path = "%s.%s.%d" % (prefix, name, index)
 			flatten_with(child_path, child, result)
 	result = {}
-	flatten_with(namespaced_name(xml_element), xml_element, result)
+	flatten_with(namespaced_name(xml_element.tag, xml_element.nsmap),
+			xml_element, result)
 	return Metadata(result)
 
 def dummy_metadata_reader(xml_element):
