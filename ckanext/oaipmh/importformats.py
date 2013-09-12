c193aa0937c7090d59295d08c6aa5cb0aa1a3814
Panu Kalliokoski <panu.kalliokoski@server.fake>
2013-09-12 15:24:38 +0300

    document attributes, file attributes, rest of common fields

--- ckanext/oaipmh/importformats.py
+++ ckanext/oaipmh/importformats.py
@@ -27,18 +27,37 @@ def copy_element(source, dest, md, callback = None):
 def nrd_metadata_reader(xml):
 	result = generic_rdf_metadata_reader(xml).getMap()
 
-	def pick_person_attributes(source, dest):
+	def person_attrs(source, dest):
 		# TODO: here we could also fetch from ISNI/ORCID
 		copy_element(source + '/foaf:name', dest + '/name', result)
 		copy_element(source + '/foaf:mbox', dest + '/email', result)
 		copy_element(source + '/foaf:phone', dest + '/phone', result)
 
-	def copy_funding(source, dest):
+	def document_attrs(source, dest):
+		copy_element(source + '/dct:title', dest + '/title', result)
+		copy_element(source + '/dct:identifier', dest, result)
+		copy_element(source + '/dct:creator',
+				dest + '/creator/name', result)
+		copy_element(source + '/nrd:creator', dest + '/creator',
+				result, person_attrs)
+		copy_element(source + '/dct:description',
+				dest + '/description', result)
+
+	def funding_attrs(source, dest):
 		copy_element(source + '/rev:arpfo:funds.0/arpfo:grantNumber.0',
 				dest + '/fundingNumber', result)
 		copy_element(source + '/rev:arpfo:funds.0/rev:arpfo:provides.0',
 				dest + '/funder', result,
-				pick_person_attributes)
+				person_attrs)
+
+	def file_attrs(source, dest):
+		copy_element(source + '/dcat:mediaType',
+				dest + '/mimetype', result)
+		copy_element(source + '/fp:checksum.0/fp:checksumValue.0',
+				dest + '/checksum.0', result)
+		copy_element(source + '/fp:checksum.0/fp:generator.0',
+				dest + '/checksum.0/algorithm', result)
+		copy_element(source + '/dcat:byteSize', dest + '/size', result)
 
 	mapping = [(u'dataset', u'versionidentifier', None),
 		(u'dataset/nrd:continuityIdentifier', u'continuityidentifier',
@@ -51,14 +70,22 @@ def nrd_metadata_reader(xml):
 		(u'dataset/nrd:modified', u'modified', None),
 		(u'dataset/nrd:rights', u'rights', None),
 		(u'dataset/nrd:language', u'language', None),
-		(u'dataset/nrd:owner', u'owner', pick_person_attributes),
-		(u'dataset/nrd:creator', u'creator', pick_person_attributes),
-		(u'dataset/nrd:distributor', u'distributor',
-			pick_person_attributes),
-		(u'dataset/nrd:contributor', u'contributor',
-			pick_person_attributes),
+		(u'dataset/nrd:owner', u'owner', person_attrs),
+		(u'dataset/nrd:creator', u'creator', person_attrs),
+		(u'dataset/nrd:distributor', u'distributor', person_attrs),
+		(u'dataset/nrd:contributor', u'contributor', person_attrs),
 		(u'dataset/nrd:subject', u'subject', None), # fetch tags?
-		(u'dataset/nrd:producerProject', u'project', copy_funding),
+		(u'dataset/nrd:producerProject', u'project', funding_attrs),
+		(u'dataset/dct:isPartOf', u'collection', document_attrs),
+		(u'dataset/dct:requires', u'requires', None),
+		(u'dataset/nrd:discipline', u'discipline', None),
+		(u'dataset/nrd:temporal', u'temporalCoverage', None),
+		(u'dataset/nrd:spatial', u'spatialCoverage', None), # names?
+		(u'dataset/nrd:manifestation', u'resource', file_attrs),
+		(u'dataset/nrd:observationMatrix', u'structure', None), # TODO
+		(u'dataset/nrd:usedByPublication', u'publication',
+			document_attrs),
+		(u'dataset/dct:description', u'description', None),
 	]
 	for source, dest, callback in mapping:
 		copy_element(source, dest, result, callback)
