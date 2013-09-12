360d0aa9515142a38692cd23c577ac30ad15f274
Panu Kalliokoski <panu.kalliokoski@server.fake>
2013-09-12 10:26:04 +0300

    more metadata elements for NRD

--- ckanext/oaipmh/importformats.py
+++ ckanext/oaipmh/importformats.py
@@ -2,8 +2,14 @@
 
 from oaipmh.common import Metadata
 from importcore import generic_xml_metadata_reader, generic_rdf_metadata_reader
+from lxml import etree
 
 def pick_metadata_element(source, dest, md, callback = None):
+	if source in md:
+		md[dest] = md[dest + '.0'] = md[source]
+		md[dest + '.count'] = 1
+		if callback: callback(source, dest)
+		return
 	count = md.get(source + '.count', 0)
 	md[dest + '.count'] = count
 	for i in range(count):
@@ -21,6 +27,18 @@ def pick_metadata_element(source, dest, md, callback = None):
 def nrd_metadata_reader(xml):
 	result = generic_rdf_metadata_reader(xml).getMap()
 	pick_metadata_element(u'dataset/dct:title', u'title', result)
+	pick_metadata_element(u'dataset/nrd:modified', u'modified', result)
+	pick_metadata_element(u'dataset/nrd:rights', u'rights', result)
+	try:
+		rights = etree.XML(result[u'rights'])
+		rightsclass = rights.attrib['RIGHTSCATEGORY'].lower()
+		result[u'rightsclass'] = rightsclass
+		if rightsclass == 'licensed':
+			result[u'license'] = rights[0].text
+		if rightsclass == 'contractual':
+			result[u'accessURL'] = rights[0].text
+	except: pass
+	pick_metadata_element(u'dataset/nrd:language', u'language', result)
 	return Metadata(result)
 
 def dc_metadata_reader(xml):
