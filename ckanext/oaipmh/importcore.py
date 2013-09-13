41f8a05b658bb88eadf6e30cf4a1c467de9c0a07
Panu Kalliokoski <panu.kalliokoski@server.fake>
2013-09-13 10:00:28 +0300

    tabs expanded

--- ckanext/oaipmh/importcore.py
+++ ckanext/oaipmh/importcore.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-
 # coding: utf-8
+# vi:et:ts=8:
 
 from oaipmh.metadata import MetadataRegistry
 from oaipmh.common import Metadata
@@ -10,137 +10,137 @@ from cStringIO import StringIO
 import os
 
 default_namespaces = [
-	('dc', 'http://purl.org/dc/elements/1.1/'),
-	('dct', 'http://purl.org/dc/terms/'),
-	('xsd', 'http://www.w3.org/2001/XMLSchema#'),
-	('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
-	('rdfs', 'http://www.w3.org/2000/01/rdf-schema#'),
-	('skos', 'http://www.w3.org/2004/02/skos/core#'),
-	('owl', 'http://www.w3.org/2002/07/owl#'),
-	('nrd', 'http://purl.org/net/nrd#'),
-	('void', 'http://rdfs.org/ns/void#'),
-	('foaf', 'http://xmlns.com/foaf/0.1/'),
-	('dcat', 'http://www.w3.org/ns/dcat#'),
-	('fp', 'http://downlode.org/Code/RDF/File_Properties/schema#'),
-	('arpfo', 'http://vocab.ox.ac.uk/projectfunding#'),
-	('org', 'http://www.w3.org/ns/org#'),
-	('lvont', 'http://lexvo.org/ontology#'),
-	('qb', 'http://purl.org/linked-data/cube#'),
-	('prov', 'http://www.w3.org/ns/prov#'),
+        ('dc', 'http://purl.org/dc/elements/1.1/'),
+        ('dct', 'http://purl.org/dc/terms/'),
+        ('xsd', 'http://www.w3.org/2001/XMLSchema#'),
+        ('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+        ('rdfs', 'http://www.w3.org/2000/01/rdf-schema#'),
+        ('skos', 'http://www.w3.org/2004/02/skos/core#'),
+        ('owl', 'http://www.w3.org/2002/07/owl#'),
+        ('nrd', 'http://purl.org/net/nrd#'),
+        ('void', 'http://rdfs.org/ns/void#'),
+        ('foaf', 'http://xmlns.com/foaf/0.1/'),
+        ('dcat', 'http://www.w3.org/ns/dcat#'),
+        ('fp', 'http://downlode.org/Code/RDF/File_Properties/schema#'),
+        ('arpfo', 'http://vocab.ox.ac.uk/projectfunding#'),
+        ('org', 'http://www.w3.org/ns/org#'),
+        ('lvont', 'http://lexvo.org/ontology#'),
+        ('qb', 'http://purl.org/linked-data/cube#'),
+        ('prov', 'http://www.w3.org/ns/prov#'),
 ]
 
 def namespaced_name(name, namespaces):
-	for prefix, nsurl in namespaces + default_namespaces:
-		if prefix is None: prefix = ''
-		else: prefix += ':'
-		if name.startswith(nsurl): return prefix + name[len(nsurl):]
-		nsurl = '{%s}' % nsurl
-		if name.startswith(nsurl): return prefix + name[len(nsurl):]
-	return name
+        for prefix, nsurl in namespaces + default_namespaces:
+                if prefix is None: prefix = ''
+                else: prefix += ':'
+                if name.startswith(nsurl): return prefix + name[len(nsurl):]
+                nsurl = '{%s}' % nsurl
+                if name.startswith(nsurl): return prefix + name[len(nsurl):]
+        return name
 
 def namepath_for_element(prefix, name, indices, md):
-	index = indices.get(name, 0)
-	indices[name] = index + 1
-	last_rel = prefix.split('/')[-1]
-	if not is_reverse_relation(name, last_rel):
-		md["%s/%s.count" % (prefix, name)] = index + 1
-	return "%s/%s.%d" % (prefix, name, index)
+        index = indices.get(name, 0)
+        indices[name] = index + 1
+        last_rel = prefix.split('/')[-1]
+        if not is_reverse_relation(name, last_rel):
+                md["%s/%s.count" % (prefix, name)] = index + 1
+        return "%s/%s.%d" % (prefix, name, index)
 
 def generic_xml_metadata_reader(xml_element):
-	def flatten_with(prefix, element, result):
-		if element.text: result[prefix] = element.text
-		for attr in element.attrib:
-			name = namespaced_name(attr, element.nsmap.items())
-			result["%s/@%s" % (prefix, name)] = element.attrib[attr]
-		indices = {}
-		for child in element:
-			name = namespaced_name(child.tag, child.nsmap.items())
-			child_path = namepath_for_element(prefix, name,
-					indices, result)
-			flatten_with(child_path, child, result)
-	result = {}
-	flatten_with(namespaced_name(xml_element.tag,
-		xml_element.nsmap.items()), xml_element, result)
-	return Metadata(result)
+        def flatten_with(prefix, element, result):
+                if element.text: result[prefix] = element.text
+                for attr in element.attrib:
+                        name = namespaced_name(attr, element.nsmap.items())
+                        result["%s/@%s" % (prefix, name)] = element.attrib[attr]
+                indices = {}
+                for child in element:
+                        name = namespaced_name(child.tag, child.nsmap.items())
+                        child_path = namepath_for_element(prefix, name,
+                                        indices, result)
+                        flatten_with(child_path, child, result)
+        result = {}
+        flatten_with(namespaced_name(xml_element.tag,
+                xml_element.nsmap.items()), xml_element, result)
+        return Metadata(result)
 
 def is_reverse_relation(rel1, rel2):
-	try: rel1 = rel1[:rel1.rindex('.')]
-	except ValueError: pass
-	try: rel2 = rel2[:rel2.rindex('.')]
-	except ValueError: pass
-	return rel1 == 'rev:' + rel2 or rel2 == 'rev:' + rel1
+        try: rel1 = rel1[:rel1.rindex('.')]
+        except ValueError: pass
+        try: rel2 = rel2[:rel2.rindex('.')]
+        except ValueError: pass
+        return rel1 == 'rev:' + rel2 or rel2 == 'rev:' + rel1
 
 def generic_rdf_metadata_reader(xml_element):
-	g = Graph()
-	e = etree.ElementTree(xml_element[0])
-	ns = dict((prefix, Namespace(nsurl))
-			for prefix, nsurl in default_namespaces)
-	# this is kinda stupid, but by far the easiest way:
-	# rdflib uses xml.sax so it doesn't understand etree,
-	# so text is the only common language spoken by lxml and rdflib
-	f = StringIO(etree.tostring(e, xml_declaration=True, encoding="utf-8"))
-	g.parse(f, format='xml') # publicID could be the metadata source URL
-	# end stupid
+        g = Graph()
+        e = etree.ElementTree(xml_element[0])
+        ns = dict((prefix, Namespace(nsurl))
+                        for prefix, nsurl in default_namespaces)
+        # this is kinda stupid, but by far the easiest way:
+        # rdflib uses xml.sax so it doesn't understand etree,
+        # so text is the only common language spoken by lxml and rdflib
+        f = StringIO(etree.tostring(e, xml_declaration=True, encoding="utf-8"))
+        g.parse(f, format='xml') # publicID could be the metadata source URL
+        # end stupid
 
-	visited = set()
-	def flatten_with(prefix, node, result):
-		path = prefix.split('/')
-		if len(path) > 2 and is_reverse_relation(path[-1], path[-2]):
-			return
-		result[prefix] = unicode(node)
-		if node in visited: return
-		visited.add(node)
-		if hasattr(node, 'language') and node.language:
-			result[prefix + '/language'] = node.language
-		indices = {}
-		arcs = [(namespaced_name(str(p), list(g.namespaces())), o)
-				for p, o in g.predicate_objects(node)] + \
-			[('rev:' + namespaced_name(str(p),
-				list(g.namespaces())), s)
-				for s, p in g.subject_predicates(node)]
-		for name, child in arcs:
-			child_path = namepath_for_element(prefix, name,
-					indices, result)
-			flatten_with(child_path, child, result)
+        visited = set()
+        def flatten_with(prefix, node, result):
+                path = prefix.split('/')
+                if len(path) > 2 and is_reverse_relation(path[-1], path[-2]):
+                        return
+                result[prefix] = unicode(node)
+                if node in visited: return
+                visited.add(node)
+                if hasattr(node, 'language') and node.language:
+                        result[prefix + '/language'] = node.language
+                indices = {}
+                arcs = [(namespaced_name(str(p), list(g.namespaces())), o)
+                                for p, o in g.predicate_objects(node)] + \
+                        [('rev:' + namespaced_name(str(p),
+                                list(g.namespaces())), s)
+                                for s, p in g.subject_predicates(node)]
+                for name, child in arcs:
+                        child_path = namepath_for_element(prefix, name,
+                                        indices, result)
+                        flatten_with(child_path, child, result)
 
-	datasets = list(g.subjects(ns['rdf']['type'], ns['nrd']['Dataset']))
-	assert len(datasets) == 1
-	root_node = datasets[0]
-	result = {}
-	flatten_with(u'dataset', root_node, result)
-	return Metadata(result)
+        datasets = list(g.subjects(ns['rdf']['type'], ns['nrd']['Dataset']))
+        assert len(datasets) == 1
+        root_node = datasets[0]
+        result = {}
+        flatten_with(u'dataset', root_node, result)
+        return Metadata(result)
 
 def dummy_metadata_reader(xml_element):
-	return Metadata({'test': 'success'})
+        return Metadata({'test': 'success'})
 
 def create_metadata_registry():
-	from importformats import nrd_metadata_reader, dc_metadata_reader
-	registry = MetadataRegistry()
-	registry.registerReader('oai_dc', dc_metadata_reader)
-	registry.registerReader('nrd', nrd_metadata_reader)
-	registry.registerReader('rdf', generic_rdf_metadata_reader)
-	return registry
+        from importformats import nrd_metadata_reader, dc_metadata_reader
+        registry = MetadataRegistry()
+        registry.registerReader('oai_dc', dc_metadata_reader)
+        registry.registerReader('nrd', nrd_metadata_reader)
+        registry.registerReader('rdf', generic_rdf_metadata_reader)
+        return registry
 
 def test_fetch(url, record_id, fmt):
-	from oaipmh.client import Client
-	registry = create_metadata_registry()
-	client = Client(url, registry)
-	record = client.getRecord(identifier=record_id, metadataPrefix=fmt)
-	return record
+        from oaipmh.client import Client
+        registry = create_metadata_registry()
+        client = Client(url, registry)
+        record = client.getRecord(identifier=record_id, metadataPrefix=fmt)
+        return record
 
 def test_list(url):
-	from oaipmh.client import Client
-	registry = create_metadata_registry()
-	client = Client(url, registry)
-	return (header.identifier() for header in
-			client.listIdentifiers(metadataPrefix='oai_dc'))
+        from oaipmh.client import Client
+        registry = create_metadata_registry()
+        client = Client(url, registry)
+        return (header.identifier() for header in
+                        client.listIdentifiers(metadataPrefix='oai_dc'))
 
 if __name__ == '__main__':
-	import sys
-	if len(sys.argv) > 3:
-		header, metadata, about = test_fetch(sys.argv[1],
-				sys.argv[2], sys.argv[3])
-		for item in metadata.getMap().items(): print item
-	else:
-		for item in test_list(sys.argv[1]): print item
+        import sys
+        if len(sys.argv) > 3:
+                header, metadata, about = test_fetch(sys.argv[1],
+                                sys.argv[2], sys.argv[3])
+                for item in metadata.getMap().items(): print item
+        else:
+                for item in test_list(sys.argv[1]): print item
 
--- ckanext/oaipmh/importformats.py
+++ ckanext/oaipmh/importformats.py
@@ -1,121 +1,122 @@
 # coding: utf-8
+# vi:et:ts=8:
 
 from oaipmh.common import Metadata
 from importcore import generic_xml_metadata_reader, generic_rdf_metadata_reader
 from lxml import etree
 
 def copy_element(source, dest, md, callback = None):
-	if source in md:
-		md[dest] = md[source]
-		copy_element(source + '/language', dest + '/language', md)
-		copy_element(source + '/@lang', dest + '/language', md)
-		copy_element(source + '/@xml:lang', dest + '/language', md)
-		if callback: callback(source, dest)
-		return
-	count = md.get(source + '.count', 0)
-	if not count: return
-	md[dest + '.count'] = count
-	for i in range(count):
-		source_n = '%s.%d' % (source, i)
-		dest_n = '%s.%d' % (dest, i)
-		copy_element(source_n, dest_n, md, callback)
+        if source in md:
+                md[dest] = md[source]
+                copy_element(source + '/language', dest + '/language', md)
+                copy_element(source + '/@lang', dest + '/language', md)
+                copy_element(source + '/@xml:lang', dest + '/language', md)
+                if callback: callback(source, dest)
+                return
+        count = md.get(source + '.count', 0)
+        if not count: return
+        md[dest + '.count'] = count
+        for i in range(count):
+                source_n = '%s.%d' % (source, i)
+                dest_n = '%s.%d' % (dest, i)
+                copy_element(source_n, dest_n, md, callback)
 
 def nrd_metadata_reader(xml):
-	result = generic_rdf_metadata_reader(xml).getMap()
+        result = generic_rdf_metadata_reader(xml).getMap()
 
-	def person_attrs(source, dest):
-		# TODO: here we could also fetch from ISNI/ORCID
-		copy_element(source + '/foaf:name', dest + '/name', result)
-		copy_element(source + '/foaf:mbox', dest + '/email', result)
-		copy_element(source + '/foaf:phone', dest + '/phone', result)
+        def person_attrs(source, dest):
+                # TODO: here we could also fetch from ISNI/ORCID
+                copy_element(source + '/foaf:name', dest + '/name', result)
+                copy_element(source + '/foaf:mbox', dest + '/email', result)
+                copy_element(source + '/foaf:phone', dest + '/phone', result)
 
-	def document_attrs(source, dest):
-		copy_element(source + '/dct:title', dest + '/title', result)
-		copy_element(source + '/dct:identifier', dest, result)
-		copy_element(source + '/dct:creator',
-				dest + '/creator.0/name', result)
-		copy_element(source + '/nrd:creator', dest + '/creator',
-				result, person_attrs)
-		copy_element(source + '/dct:description',
-				dest + '/description', result)
+        def document_attrs(source, dest):
+                copy_element(source + '/dct:title', dest + '/title', result)
+                copy_element(source + '/dct:identifier', dest, result)
+                copy_element(source + '/dct:creator',
+                                dest + '/creator.0/name', result)
+                copy_element(source + '/nrd:creator', dest + '/creator',
+                                result, person_attrs)
+                copy_element(source + '/dct:description',
+                                dest + '/description', result)
 
-	def funding_attrs(source, dest):
-		copy_element(source + '/rev:arpfo:funds.0/arpfo:grantNumber',
-				dest + '/fundingNumber', result)
-		copy_element(source + '/rev:arpfo:funds.0/rev:arpfo:provides',
-				dest + '/funder', result,
-				person_attrs)
+        def funding_attrs(source, dest):
+                copy_element(source + '/rev:arpfo:funds.0/arpfo:grantNumber',
+                                dest + '/fundingNumber', result)
+                copy_element(source + '/rev:arpfo:funds.0/rev:arpfo:provides',
+                                dest + '/funder', result,
+                                person_attrs)
 
-	def file_attrs(source, dest):
-		copy_element(source + '/dcat:mediaType',
-				dest + '/mimetype', result)
-		copy_element(source + '/fp:checksum.0/fp:checksumValue.0',
-				dest + '/checksum.0', result)
-		copy_element(source + '/fp:checksum.0/fp:generator.0',
-				dest + '/checksum.0/algorithm', result)
-		copy_element(source + '/dcat:byteSize', dest + '/size', result)
+        def file_attrs(source, dest):
+                copy_element(source + '/dcat:mediaType',
+                                dest + '/mimetype', result)
+                copy_element(source + '/fp:checksum.0/fp:checksumValue.0',
+                                dest + '/checksum.0', result)
+                copy_element(source + '/fp:checksum.0/fp:generator.0',
+                                dest + '/checksum.0/algorithm', result)
+                copy_element(source + '/dcat:byteSize', dest + '/size', result)
 
-	mapping = [(u'dataset', u'versionidentifier', None),
-		(u'dataset/nrd:continuityIdentifier', u'continuityidentifier',
-			None),
-		(u'dataset/rev:foaf:primaryTopic.0/nrd:metadataIdentifier',
-			u'metadata/identifier', None),
-		(u'dataset/rev:foaf:primaryTopic.0/nrd:metadataModified',
-			u'metadata/modified', None),
-		(u'dataset/dct:title', u'title', None),
-		(u'dataset/nrd:modified', u'modified', None),
-		(u'dataset/nrd:rights', u'rights', None),
-		(u'dataset/nrd:language', u'language', None),
-		(u'dataset/nrd:owner', u'owner', person_attrs),
-		(u'dataset/nrd:creator', u'creator', person_attrs),
-		(u'dataset/nrd:distributor', u'distributor', person_attrs),
-		(u'dataset/nrd:contributor', u'contributor', person_attrs),
-		(u'dataset/nrd:subject', u'subject', None), # fetch tags?
-		(u'dataset/nrd:producerProject', u'project', funding_attrs),
-		(u'dataset/dct:isPartOf', u'collection', document_attrs),
-		(u'dataset/dct:requires', u'requires', None),
-		(u'dataset/nrd:discipline', u'discipline', None),
-		(u'dataset/nrd:temporal', u'temporalcoverage', None),
-		(u'dataset/nrd:spatial', u'spatialcoverage', None), # names?
-		(u'dataset/nrd:manifestation', u'resource', file_attrs),
-		(u'dataset/nrd:observationMatrix', u'variables', None), # TODO
-		(u'dataset/nrd:usedByPublication', u'publication',
-			document_attrs),
-		(u'dataset/dct:description', u'description', None),
-	]
-	for source, dest, callback in mapping:
-		copy_element(source, dest, result, callback)
-	try:
-		rights = etree.XML(result[u'rights'])
-		rightsclass = rights.attrib['RIGHTSCATEGORY'].lower()
-		result[u'rightsclass'] = rightsclass
-		if rightsclass == 'licensed':
-			result[u'license'] = rights[0].text
-		if rightsclass == 'contractual':
-			result[u'accessURL'] = rights[0].text
-	except: pass
-	return Metadata(result)
+        mapping = [(u'dataset', u'versionidentifier', None),
+                (u'dataset/nrd:continuityIdentifier', u'continuityidentifier',
+                        None),
+                (u'dataset/rev:foaf:primaryTopic.0/nrd:metadataIdentifier',
+                        u'metadata/identifier', None),
+                (u'dataset/rev:foaf:primaryTopic.0/nrd:metadataModified',
+                        u'metadata/modified', None),
+                (u'dataset/dct:title', u'title', None),
+                (u'dataset/nrd:modified', u'modified', None),
+                (u'dataset/nrd:rights', u'rights', None),
+                (u'dataset/nrd:language', u'language', None),
+                (u'dataset/nrd:owner', u'owner', person_attrs),
+                (u'dataset/nrd:creator', u'creator', person_attrs),
+                (u'dataset/nrd:distributor', u'distributor', person_attrs),
+                (u'dataset/nrd:contributor', u'contributor', person_attrs),
+                (u'dataset/nrd:subject', u'subject', None), # fetch tags?
+                (u'dataset/nrd:producerProject', u'project', funding_attrs),
+                (u'dataset/dct:isPartOf', u'collection', document_attrs),
+                (u'dataset/dct:requires', u'requires', None),
+                (u'dataset/nrd:discipline', u'discipline', None),
+                (u'dataset/nrd:temporal', u'temporalcoverage', None),
+                (u'dataset/nrd:spatial', u'spatialcoverage', None), # names?
+                (u'dataset/nrd:manifestation', u'resource', file_attrs),
+                (u'dataset/nrd:observationMatrix', u'variables', None), # TODO
+                (u'dataset/nrd:usedByPublication', u'publication',
+                        document_attrs),
+                (u'dataset/dct:description', u'description', None),
+        ]
+        for source, dest, callback in mapping:
+                copy_element(source, dest, result, callback)
+        try:
+                rights = etree.XML(result[u'rights'])
+                rightsclass = rights.attrib['RIGHTSCATEGORY'].lower()
+                result[u'rightsclass'] = rightsclass
+                if rightsclass == 'licensed':
+                        result[u'license'] = rights[0].text
+                if rightsclass == 'contractual':
+                        result[u'accessURL'] = rights[0].text
+        except: pass
+        return Metadata(result)
 
 def dc_metadata_reader(xml):
-	result = generic_xml_metadata_reader(xml).getMap()
-	mapping = [(u'dc:title', u'title.%d'),
-		(u'dc:identifier', u'versionidentifier.%d'),
-		(u'dc:creator', u'creator.%d/name.0'),
-		(u'dc:language', u'language.%d/label.0'),
-		(u'dc:description', u'description.%d'),
-		(u'dc:subject', u'subject.%d'),
-		(u'dc:publisher', u'distributor.%d/name.0'),
-		(u'dc:format', u'resource.%d/format.0'),
-		(u'dc:contributor', u'contributor.%d/name.0'),
-		(u'dc:rights', u'license.%d/description.0'),
-		(u'dc:source', u'continuityidentifier.%d'),
-	]
-	for source, dest in mapping:
-		count = result.get('metadata/oai_dc:dc.0/%s.count' % source, 0)
-		result[dest[:dest.index('.%d')] + '.count'] = count
-		for i in range(count):
-			source_n = 'metadata/oai_dc:dc.0/%s.%d' % (source, i)
-			copy_element(source_n, dest % i, result)
-			if dest.endswith('.0'):
-				result[dest[:-2] % i + '.count'] = 1
-	return Metadata(result)
+        result = generic_xml_metadata_reader(xml).getMap()
+        mapping = [(u'dc:title', u'title.%d'),
+                (u'dc:identifier', u'versionidentifier.%d'),
+                (u'dc:creator', u'creator.%d/name.0'),
+                (u'dc:language', u'language.%d/label.0'),
+                (u'dc:description', u'description.%d'),
+                (u'dc:subject', u'subject.%d'),
+                (u'dc:publisher', u'distributor.%d/name.0'),
+                (u'dc:format', u'resource.%d/format.0'),
+                (u'dc:contributor', u'contributor.%d/name.0'),
+                (u'dc:rights', u'license.%d/description.0'),
+                (u'dc:source', u'continuityidentifier.%d'),
+        ]
+        for source, dest in mapping:
+                count = result.get('metadata/oai_dc:dc.0/%s.count' % source, 0)
+                result[dest[:dest.index('.%d')] + '.count'] = count
+                for i in range(count):
+                        source_n = 'metadata/oai_dc:dc.0/%s.%d' % (source, i)
+                        copy_element(source_n, dest % i, result)
+                        if dest.endswith('.0'):
+                                result[dest[:-2] % i + '.count'] = 1
+        return Metadata(result)
