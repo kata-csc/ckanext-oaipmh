90b5b6620157b80752dcaf259e3d17a279c93c29
Mikko Koho <mikko.koho@csc.fi>
2014-01-13 15:03:40 +0200

    Some simple tests and a script to run them

---
+++ ckanext/oaipmh/tests/test_oai.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+#
+# pylint: disable=no-self-use, missing-docstring, too-many-public-methods, invalid-name
+
+"""
+Test classes for OAI-PMH harvester.
+"""
+
+import ckan
+
+from unittest import TestCase
+from ckanext.oaipmh.harvester import OAIPMHHarvester
+import ckanext.harvest.model as harvest_model
+from ckanext.kata import model as kata_model
+
+class Test_OAIPMHHarvester(TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        '''
+        Setup database and variables
+        '''
+        harvest_model.setup()
+        kata_model.setup()
+        cls.harvester = OAIPMHHarvester()
+
+    def test_get_field_titles(self):
+        info = self.harvester.info()
+        assert isinstance(info, dict)
+
+    def test_gather_stage(self):
+        # gather_stage should throw some exception with parameter None
+        self.assertRaises(Exception, self.harvester.gather_stage, (None))
+
+    def test_fetch_stage(self):
+        # should throw some exception with parameter None
+        self.assertRaises(Exception, self.harvester.fetch_stage, (None))
+
+    @classmethod
+    def teardown_class(cls):
+        ckan.model.repo.rebuild_db()
+
---
+++ nose.sh
@@ -0,0 +1,2 @@
+echo "Running tests for OAI-PMH Harvester. (You should run this from src/)";
+nosetests --ckan --with-pylons=ckanext-oaipmh/test-core.ini ckanext-oaipmh/ckanext/oaipmh/tests --logging-level=CRITICAL
