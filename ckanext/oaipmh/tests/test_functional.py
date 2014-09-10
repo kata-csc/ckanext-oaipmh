b1a2d63790162223cde3faae4ad5edd38f6511e0
Ilja <ilja.pyykkonen@gofore.com>
2014-09-10 10:41:43 +0300

    Test for OAI-PMH-server.

--- ckanext/oaipmh/tests/test_functional.py
+++ ckanext/oaipmh/tests/test_functional.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 #
 # pylint: disable=no-self-use, missing-docstring, too-many-public-methods, invalid-name
-
 """
 Functional tests for OAI-PMH harvester.
 """
@@ -17,8 +16,19 @@ from ckanext.oaipmh import importformats
 from ckanext.oaipmh.harvester import OAIPMHHarvester
 import ckanext.kata.model as kata_model
 
+from ckan.tests import WsgiAppCase
+from ckan.lib.helpers import url_for
+
+import lxml.etree
+from ckan.logic import get_action
+from ckan import model
+from ckanext.kata.tests.test_fixtures.unflattened import TEST_DATADICT
+from copy import deepcopy
+
+
 FIXTURE_LISTIDENTIFIERS = "file:ckanext-oaipmh/ckanext/oaipmh/test_fixtures/listidentifiers.xml"
 FIXTURE_DATASET = "file:ckanext-oaipmh/ckanext/oaipmh/test_fixtures/oai-pmh.xml"
+
 
 class TestReadingFixtures(TestCase):
 
@@ -32,6 +42,10 @@ class TestReadingFixtures(TestCase):
         harvest_model.setup()
         kata_model.setup()
         cls.harvester = OAIPMHHarvester()
+
+    @classmethod
+    def teardown_class(cls):
+        ckan.model.repo.rebuild_db()
 
     def test_list(self):
         '''
@@ -68,6 +82,73 @@ class TestReadingFixtures(TestCase):
         client = oaipmh.client.Client(FIXTURE_LISTIDENTIFIERS, registry)
         self.assertRaises(Exception, getrecord)
 
-    @classmethod
-    def teardown_class(cls):
-        ckan.model.repo.rebuild_db()
+
+class TestOaipmhServer(WsgiAppCase, TestCase):
+    """ Test OAI-PMH server """
+
+    _namespaces = {'o': 'http://www.openarchives.org/OAI/2.0/',
+                   'oai_dc': "http://www.openarchives.org/OAI/2.0/oai_dc/",
+                   'dc': "http://purl.org/dc/elements/1.1/"}
+
+    def _get_single_result(self, xml, xpath):
+        results = xml.xpath(xpath, namespaces=self._namespaces)
+        self.assertEquals(len(results), 1)
+        return results[0]
+
+    def test_records(self):
+        """ Test record fetching via http-request to prevent accidental changes to interface """
+        model.User(name="test", sysadmin=True).save()
+        organization = get_action('organization_create')({'user': 'test'}, {'name': 'test-organization', 'title': "Test organization"})
+        package_1_data = deepcopy(TEST_DATADICT)
+        package_1_data['owner_org'] = organization['name']
+        package_1_data['private'] = False
+        package_2_data = deepcopy(package_1_data)
+
+        package_1_data['name'] = 'test-package1'
+        package_2_data['name'] = 'test-package2'
+
+        packages = [get_action('package_create')({'user': 'test'}, package_1_data),
+                    get_action('package_create')({'user': 'test'}, package_2_data)]
+
+        package_identifiers = [package['id'] for package in packages]
+        package_names = [package['name'] for package in packages]
+
+        url = url_for('/oai')
+        result = self.app.get(url, {'verb': 'ListSets'})
+
+        root = lxml.etree.fromstring(result.body)
+        request_set = self._get_single_result(root, "//o:set")
+        set_name = request_set.xpath("string(o:setName)", namespaces=self._namespaces)
+        set_spec = request_set.xpath("string(o:setSpec)", namespaces=self._namespaces)
+        self.assertEquals(organization['id'], set_spec)
+        self.assertEquals(organization['name'], set_name)
+
+        result = self.app.get(url, {'verb': 'ListIdentifiers', 'set': set_spec, 'metadataPrefix': 'oai_dc' })
+
+        root = lxml.etree.fromstring(result.body)
+        fail = True
+
+        for header in root.xpath("//o:header", namespaces=self._namespaces):
+            fail = False
+            set_spec = header.xpath("string(o:setSpec)", namespaces=self._namespaces)
+            identifier = header.xpath("string(o:identifier)", namespaces=self._namespaces)
+            self.assertTrue(set_spec in package_names)
+            self.assertTrue(identifier in package_identifiers)
+
+            result = self.app.get(url, {'verb': 'GetRecord', 'identifier': identifier, 'metadataPrefix': 'oai_dc' })
+
+            root = lxml.etree.fromstring(result.body)
+
+            fail_record = True
+            for record_result in root.xpath("//o:record", namespaces=self._namespaces):
+                fail_record = False
+                header = self._get_single_result(record_result, 'o:header')
+                metadata = self._get_single_result(record_result, 'o:metadata')
+
+                self.assertTrue(header.xpath("string(o:identifier)", namespaces=self._namespaces) in package_identifiers)
+                self.assertTrue(header.xpath("string(o:setSpec)", namespaces=self._namespaces) in package_names)
+                self.assertTrue(metadata.xpath("string(oai_dc:dc/dc:title)", namespaces=self._namespaces) in package_names)
+
+            self.assertFalse(fail_record, "No records received")
+
+        self.assertFalse(fail, "No headers (packages) received")
