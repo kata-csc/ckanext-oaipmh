4b34812277aed7c92ec07729041c83d54ac5bab7
Mikko Koho <mikko.koho@csc.fi>
2015-03-13 12:34:40 +0200

    Update nose.sh

--- nose.sh
+++ nose.sh
@@ -1,2 +1,2 @@
 echo "Running tests for OAI-PMH Harvester. (You should run this from src/)";
-nosetests --ckan --with-pylons=ckanext-oaipmh/test-core.ini ckanext-oaipmh/ckanext/oaipmh/tests --logging-level=CRITICAL
+nosetests --ckan --with-pylons=ckanext-oaipmh/test-core.ini ckanext-oaipmh/ckanext/oaipmh/tests --logging-clear-handlers --logging-filter=ckanext
