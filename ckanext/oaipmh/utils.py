c2b5bf78719aa339fc931cec0be5090ee14670e2
Juho Lehtonen <juho.lehtonen@csc.fi>
2017-07-11 10:12:20 +0300

    OAI server: Fix 'Identify' verb on empty database.

--- ckanext/oaipmh/utils.py
+++ ckanext/oaipmh/utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 from iso639 import languages
 
 import ckan.model as model
@@ -25,6 +27,5 @@ def get_earliest_datestamp():
     Return earliest datestamp of packages as defined in:
     http://www.openarchives.org/OAI/openarchivesprotocol.html#Identify
     '''
-
-    return model.Session.query(model.Package.metadata_modified).\
-        order_by(model.Package.metadata_modified).first()[0]
+    ds = model.Session.query(model.Package.metadata_modified).order_by(model.Package.metadata_modified).first()
+    return ds[0] if ds else datetime.datetime.today()
